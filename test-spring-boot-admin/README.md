1aa479adb29660ed4251abdf1e697cabef0b04f4
Pinguet62 <pinguet62@gmail.com>
2017-07-09 21:37:31 +0200

    Spring Boot admin

---
+++ test-spring-boot-admin/README.md
@@ -0,0 +1,5 @@
+# Test
+
+## Spring Boot admin
+
+See https://github.com/codecentric/spring-boot-admin
---
+++ test-spring-boot-admin/test-spring-boot-admin-server/src/main/java/fr/pinguet62/test/springbootadmin/SpringBootAdminServerApplication.java
@@ -0,0 +1,16 @@
+package fr.pinguet62.test.springbootadmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+
+@SpringBootApplication
+@EnableAdminServer
+public class SpringBootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+
+}
