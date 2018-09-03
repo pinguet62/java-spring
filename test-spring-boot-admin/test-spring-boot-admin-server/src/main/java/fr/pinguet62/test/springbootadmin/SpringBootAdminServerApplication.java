cd5a1b3198d746e91c2822d815dd21f0cfad686c
PINGUET Julien <external.z14jping@decathlon.com>
2018-09-03 14:02:38 +0200

    Version upgrade

--- test-spring-boot-admin/test-spring-boot-admin-server/src/main/java/fr/pinguet62/test/springbootadmin/SpringBootAdminServerApplication.java
+++ test-spring-boot-admin/test-spring-boot-admin-server/src/main/java/fr/pinguet62/test/springbootadmin/SpringBootAdminServerApplication.java
@@ -3,7 +3,7 @@ package fr.pinguet62.test.springbootadmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import de.codecentric.boot.admin.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
