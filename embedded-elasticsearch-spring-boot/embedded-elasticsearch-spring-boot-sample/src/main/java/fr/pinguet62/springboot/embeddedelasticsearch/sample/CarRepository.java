597b6d74db1c1325d8b0ad854e1dc30e132f889a
PINGUET Julien <pinguet62@gmail.com>
2024-04-28 10:45:55 +0200

    old project

--- embedded-elasticsearch-spring-boot/embedded-elasticsearch-spring-boot-sample/src/main/java/fr/pinguet62/springboot/embeddedelasticsearch/sample/CarRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.pinguet62.springboot.embeddedelasticsearch.sample;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CarRepository extends ElasticsearchRepository<CarDocument, String> {
-}
--- embedded-elasticsearch-spring-boot/embedded-elasticsearch-spring-boot-sample/src/main/java/fr/pinguet62/springboot/embeddedelasticsearch/sample/SampleConfig.java
+++
@@ -1,7 +0,0 @@
-package fr.pinguet62.springboot.embeddedelasticsearch.sample;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class SampleConfig {
-}
